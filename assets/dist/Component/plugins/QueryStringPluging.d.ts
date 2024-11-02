d006e9ea168f820b7786e108e482f33d772e4eb3
Hugo Alliaume <hugo@alliau.me>
2024-11-02 11:13:04 +0100

    [DX] Rework building process, and add build/watch scripts to packages package.json

--- assets/dist/Component/plugins/QueryStringPluging.d.ts
+++
@@ -1,9 +0,0 @@
-import Component from '../index';
-import { PluginInterface } from './PluginInterface';
-export default class implements PluginInterface {
-    private element;
-    private mapping;
-    attachToComponent(component: Component): void;
-    private registerBindings;
-    private updateUrl;
-}
